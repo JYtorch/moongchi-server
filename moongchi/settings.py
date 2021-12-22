76e79ef4b44fc4424d02b58630e411df8a3fe091
JYA <abogado2113@gmail.com>
2021-12-23 02:18:11 +0900

    update

--- moongchi/settings.py
+++ moongchi/settings.py
@@ -28,8 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 # DEBUG = True
 
-ALLOWED_HOSTS = [
-    'https://moongchi.netlify.app',
+ALLOWED_HOSTS = [    
     'moongchi-server.herokuapp.com',
     '127.0.0.1'
 ]
@@ -147,6 +146,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'accounts.User'
 
 CORS_ALLOWED_ORIGINS = [
+    'https://moongchi.netlify.app',
     'http://localhost:8080',
 ]
 
