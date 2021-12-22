357e85380c5a59f09d724ec482bd6b259e54a053
JYA <abogado2113@gmail.com>
2021-12-23 01:49:56 +0900

    update

--- moongchi/settings.py
+++ moongchi/settings.py
@@ -28,7 +28,10 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 # DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    'moongchi-server.herokuapp.com',
+    '127.0.0.1'
+]
 
 
 # Application definition
