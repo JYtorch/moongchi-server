989b00b48692d8ddaec72087ab061795c7cdb934
JYA <abogado2113@gmail.com>
2022-10-17 10:02:38 +0900

    Update settings

--- moongchi/settings.py
+++ moongchi/settings.py
@@ -101,22 +101,22 @@ WSGI_APPLICATION = 'moongchi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-# if os.getenv("DJANGO_DATABASE_URL", None) is None:
-#     raise Exception("DJANGO_DATABASE_URL environment variable not defined")
-# DB_PARAMS = dj_database_url.parse(os.environ.get("DJANGO_DATABASE_URL"))
-# DB_PARAMS["NAME"] = "moongchi"
-# DB_PARAMS["ENGINE"] = "custom_db_backends.vitess"
-# DB_PARAMS["OPTIONS"] = {"ssl": "true", "charset": "utf8mb4"}
-
 # DATABASES = {
-#     "default": DB_PARAMS,
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
 # }
+if os.getenv("DJANGO_DATABASE_URL", None) is None:
+    raise Exception("DJANGO_DATABASE_URL environment variable not defined")
+DB_PARAMS = dj_database_url.parse(os.environ.get("DJANGO_DATABASE_URL"))
+DB_PARAMS["NAME"] = "moongchi"
+DB_PARAMS["ENGINE"] = "custom_db_backends.vitess"
+DB_PARAMS["OPTIONS"] = {"ssl": "true", "charset": "utf8mb4"}
+
+DATABASES = {
+    "default": DB_PARAMS,
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
